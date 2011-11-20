88a3165f84ecca39b3d3f75829f935e04914f5ac
Eric Teubert <ericteubert@googlemail.com>
2011-11-20 13:27:15 +0100

    autoload all files in lib directory

--- multi-feed-reader.php
+++ multi-feed-reader.php
@@ -57,9 +57,30 @@ namespace MultiFeedReader\Settings;
 const HANDLE = 'multi_feed_reader_handle';
 
 /**
- * @todo autoload all files in /lib
+ * @todo move in a bootstrapping file
  */
-require_once 'lib/tabs.php';
+// autoload all files in /lib
+spl_autoload_register( function ( $class_name ) { 
+	// get class name without namespace
+	$class_name = strtolower( array_pop( explode( '\\', $class_name ) ) );
+	// library directory
+	$lib = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR;
+	
+	// register all possible paths for the class
+	$possibilities = array(
+		$lib . $class_name . '.php'
+	);
+	
+	// search for the class
+	foreach ( $possibilities as $file ) {
+		if ( file_exists( $file ) ) {
+			require_once( $file );
+			return true;
+		}
+	}
+	
+	return false;
+});
 
 function initialize() {
 	$tabs = new \MultiFeedReader\Lib\Tabs;
